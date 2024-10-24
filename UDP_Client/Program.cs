fd943e2d987726d6d31cce89fc699dae280bbe0b
Haseung-Song <tkrmtkrm1245@naver.com>
2024-10-24 22:00:17 +0900

    [UDP 클라이언트 통신 코드] (수정)

--- UDP_Client/Program.cs
+++ UDP_Client/Program.cs
@@ -10,138 +10,153 @@ namespace UDP_Client
         public static async Task StartClient()
         {
             UdpClient udpClient = new UdpClient();
-            udpClient.Connect("127.0.0.1", 2000);
+            udpClient.Connect("127.0.0.1", 20000);
+
             try
             {
                 Console.WriteLine("UDP Client Started!");
-                byte cmdCounter = 0x00; // CMD Counter 초기값
+                byte cmdCounter = 0x00;
                 if (udpClient == null)
                 {
                     udpClient = new UdpClient();
                 }
-                int angleFlap = -40;
-                int angleTilt = 0;
-                int knobSpeed = 0;
-                int knobAltitude = 0;
-                int knobHeading = 0;
-                int stickThrottle = 0;
-                int stickRoll = -100;
-                int stickPitch = -100;
-                int stickYaw = -100;
+                int flapAngle = 0;
+                int tiltAngle = 0;
+                uint knobSpeed = 0;
+                uint knobAltitude = 0;
+                uint knobHeading = 0;
+                uint stickThrottle = 0;
+                uint stickRoll = 0;
+                uint stickPitch = 0;
+                uint stickYaw = 0;
+                uint lonOfLP = 0;
+                uint latOfLP = 0;
+                ushort altOfLP = 0;
                 while (true)
                 {
-                    for (int j = 0; j <= 80; j++) // 80번 루프 (테스트 반복)
+                    for (int j = 0; j <= 10; j++) // 총 10번 루프 (테스트 반복)
                     {
-                        byte[] message = new byte[31]; // 메시지의 바이트 크기 (5 Byte ~ 30 Byte)
-                        message[0] = 0xAF; // Header: Frame Sync (첫 번째 바이트)
-                        message[1] = 0x01; // Header: Destination Address (목적지 주소)
-                        message[2] = 0x0A; // Header: Source Address (출발지 주소)
-                        message[3] = cmdCounter; // CMD Counter: [0x00 ~ 0xFF]
+                        byte[] message = new byte[32]; // message 바이트 수: (0 Byte ~ 32 Byte)
+                        message[0] = 0xAF;             // Header: Frame Sync (첫 번째 바이트)
+                        message[1] = 0x01;             // Header: Destination Address (목적지 주소)
+                        message[2] = 0x0A;             // Header: Source Address (출발지 주소)
+                        message[3] = cmdCounter;       // CMD Counter: [0x00 ~ 0xFF]
 
-                        // ModeOverride (7번째 비트 설정: 0x80 or 0x00)
-                        if (j % 2 == 0)  // 짝수일 때 (ModeOverride = ON)
+                        // 짝수일 때  (= ON)
+                        if (j % 2 == 0)
                         {
-                            message[4] = (byte)(message[4] | 0x80); // 7번째 비트를 1로 설정 (Byte #4.)
-                            message[5] = (byte)(message[5] | 0x80); // 7번째 비트를 1로 설정 (Byte #5.)
-                            message[6] = (byte)(message[6] | 0x80); // 7번째 비트를 1로 설정 (Byte #6.)
+                            message[4] = (byte)(message[4] | 0x80);   // 7번째 비트를 1로 설정  (Byte #5.)
+                            message[5] = (byte)(message[5] | 0x80);   // 7번째 비트를 1로 설정  (Byte #6.)
+                            message[6] = (byte)(message[6] | 0x80);   // 7번째 비트를 1로 설정  (Byte #7.)
+                            message[24] = (byte)(message[24] | 0x80); // 7번째 비트를 1로 설정 (Byte #25.)
+                            message[24] = (byte)(message[24] | 0x01); // 0번째 비트를 1로 설정 (Byte #25.)
                         }
-                        else  // 홀수일 때 (ModeOverride = OFF)
+                        // 홀수일 때 (= OFF)
+                        else
                         {
-                            message[4] = (byte)(message[4] & 0x7F); // 7번째 비트를 0으로 설정 (Byte #4.)
-                            message[5] = (byte)(message[5] & 0x7F); // 7번째 비트를 0으로 설정 (Byte #5.)
-                            message[6] = (byte)(message[6] & 0x7F); // 7번째 비트를 0으로 설정 (Byte #6.)
+                            message[4] = (byte)(message[4] & 0x7F);    // 7번째 비트를 0으로 설정  (Byte #5.)
+                            message[5] = (byte)(message[5] & 0x7F);    // 7번째 비트를 0으로 설정  (Byte #6.)
+                            message[6] = (byte)(message[6] & 0x7F);    // 7번째 비트를 0으로 설정  (Byte #7.)
+                            message[24] = (byte)(message[24] & ~0x80); // 7번째 비트를 0으로 설정 (Byte #25.)
+                            message[24] = (byte)(message[24] & ~0x01); // 0번째 비트를 0으로 설정 (Byte #25.)
                         }
 
-                        // FlightMode (6~5번째 비트 설정)
-                        int flightMode = j % 4; // FlightMode는 0, 1, 2, 3 순환
-                        message[4] = (byte)(message[4] | (flightMode << 5)); // 6~5번째 비트에 FlightMode 값 저장
+                        // [FlightMode]는 [0, 1, 2, 3]까지 순환
+                        int flightMode = j % 4;
+                        message[4] = (byte)(message[4] | (flightMode << 5));       // FlightMode (6 ~ 5번째 비트 설정) (Byte #5.)
 
-                        // ModeEngage (4~1번째 비트 설정)
-                        int modeEngage = j % 9; // ModeEngage는 0, 1, 2, 3, 4, 5, 6, 7, 8 순환
-                        message[4] = (byte)(message[4] | (modeEngage << 1));
+                        // [ModeEngage]는 [0, 1, 2, 3, 4, 5, 6, 7, 8]까지 순환
+                        int modeEngage = j % 9;
+                        message[4] = (byte)(message[4] | (modeEngage << 1));       // ModeEngage (4 ~ 1번째 비트 설정) (Byte #5.)
+                        message[5] = (byte)(message[5] | (flapAngle << 1));        // FlapAngle  (6 ~ 1번째 비트 설정) (Byte #6.)
+                        message[6] = (byte)(message[6] | (tiltAngle));             // TiltAngle  (6 ~ 0번째 비트 설정) (Byte #7.)
 
-                        // FlapAngle  (6~1번째 비트 설정)
-                        int flapAngle = (angleFlap + 40) / 2;
-                        message[5] = (byte)(message[5] | (flapAngle << 1));
+                        message[7] = (byte)(knobSpeed / 1);      // KnobSpeed      (Byte #8.)
+                        message[8] = (byte)(knobAltitude / 15);  // knobAltitude   (Byte #9.)
+                        message[9] = (byte)(knobHeading / 2);    // knobHeading    (Byte #10.)
 
-                        // TiltAngle  (6~0번째 비트 설정)
-                        message[6] = (byte)(message[6] | angleTilt);
+                        message[10] = (byte)(stickThrottle);     // StickThrottle  (Byte #11.)
+                        message[11] = (byte)(stickRoll);         // StickRoll      (Byte #12.)
+                        message[12] = (byte)(stickPitch);        // StickPitch     (Byte #13.)
+                        message[13] = (byte)(stickYaw);          // StickYaw       (Byte #14.)
 
-                        message[7] = (byte)(knobSpeed);          // KnobSpeed    (8 바이트 설정)
-                        message[8] = (byte)(knobAltitude / 15);  // knobAltitude (9 바이트 설정)
-                        message[9] = (byte)(knobHeading / 2);    // knobHeading (10 바이트 설정)
+                        byte[] lonOfLPBytes = BitConverter.GetBytes(lonOfLP); // 경도값을 4 바이트로 변환
+                        Array.Copy(lonOfLPBytes, 0, message, 14, 4);  // message[14] ~ message[17]에 복사 [Byte #15. ~ Byte #18.]  = [4 Byte]
 
-                        message[10] = (byte)(stickThrottle);     // StickThrottle (11 바이트 설정)
+                        byte[] latOfLPBytes = BitConverter.GetBytes(latOfLP); // 위도값을 4 바이트로 변환
+                        Array.Copy(latOfLPBytes, 0, message, 18, 4);  // message[18] ~ message[22]에 복사 [Byte #19. ~ Byte #22.]  = [4 Byte]
 
-                        // 정수 값을 0.01 단위로 변환 ([-1 ~ 1] 범위 변환) (StickRoll)
-                        double stickRollToDouble = stickRoll * 0.01;
-                        // [-1 ~ 1] => [0X00 ~ 0XC8] 변환
-                        int rollStick = (int)((stickRollToDouble + 1) * 100);
-                        message[11] = (byte)(rollStick);         // StickRoll     (12 바이트 설정)
+                        byte[] altOfLPBytes = BitConverter.GetBytes(altOfLP); // 고도값을 2 바이트로 변환
+                        Array.Copy(altOfLPBytes, 0, message, 23, 2);  // message[23] ~ message[24]에 복사 [Byte #23. ~ Byte #24.]  = [2 Byte]
 
-                        // 정수 값을 0.01 단위로 변환 ([-1 ~ 1] 범위 변환) (StickPitch)
-                        double stickPitchToDouble = stickPitch * 0.01;
-                        // [-1 ~ 1] => [0X00 ~ 0XC8] 변환
-                        int pitchStick = (int)((stickPitchToDouble + 1) * 100);
-                        message[12] = (byte)(pitchStick);
+                        await udpClient.SendAsync(message, message.Length); // 메시지 송신 (Client => Server)
 
-                        // 정수 값을 0.01 단위로 변환 ([-1 ~ 1] 범위 변환) (StickYaw)
-                        double stickYawToDouble = stickYaw * 0.01;
-                        // [-1 ~ 1] => [0X00 ~ 0XC8] 변환
-                        int yawStick = (int)((stickYawToDouble + 1) * 100);
-                        message[13] = (byte)(yawStick);
+                        await Task.Delay(200); // 메시지 송신 후, 0.2초 지연
 
-                        await udpClient.SendAsync(message, message.Length); // 메시지 전송
-                        await Task.Delay(100); // 메시지 전송 후 0.1초 지연
+                        cmdCounter = (byte)((cmdCounter + 1) % 256); // CMD Counter 값 [0 ~ 255] 순환
 
-                        // CMD Counter 증가 및 0xFF ,후 0으로 돌아감
-                        cmdCounter = (byte)((cmdCounter + 1) % 256);
+                        // [플랩각°(도)] 2만큼 증가
+                        flapAngle += 2;
+                        // [0 ~ 40] 순환
+                        if (flapAngle > 40) flapAngle = 0;
 
-                        // 각도 2만큼 증가
-                        angleFlap += 2;
-                        // [-40도 ~ 40도] 순환
-                        if (angleFlap > 40) angleFlap = -40;
-
-                        // 각도 1만큼 증가
-                        angleTilt += 1;
+                        // [틸트각°(도)] 1만큼 증가
+                        tiltAngle += 1;
                         // [0도 ~ 90도] 순환
-                        if (angleTilt > 90) angleTilt = 0;
+                        if (tiltAngle > 90) tiltAngle = 0;
 
-                        // 속도(km/h) 1만큼 증가
+                        // [노브 속도(km/h)] 1만큼 증가
                         knobSpeed += 1;
-                        // [0km/h ~ 250km/h] 순환
+                        // [0 ~ 250] 순환
                         if (knobSpeed > 250) knobSpeed = 0;
 
-                        // 고도(m) 15만큼 증가
+                        // [노브 고도(m)] 15만큼 증가
                         knobAltitude += 15;
-                        // [0m ~ 3000m] 순환
+                        // [0 ~ 3000] 순환
                         if (knobAltitude > 3000) knobAltitude = 0;
 
-                        // 각도 2만큼 증가
+                        // [노브 방위°(도)] 2만큼 증가
                         knobHeading += 2;
-                        // [0도 ~ 358도] 순환
+                        // [0 ~ 358] 순환
                         if (knobHeading > 358) knobHeading = 0;
 
-                        // 고도 1만큼 증가
+                        // [스틱 고도] 1만큼 증가
                         stickThrottle += 1;
-                        double stickThrottleToDouble = stickThrottle * 0.005;
-                        // [0 ~ 1] 순환
-                        if (stickThrottleToDouble > 1) stickThrottle = 0;
+                        // [0 ~ 200] 순환
+                        if (stickThrottle > 200) stickThrottle = 0;
 
-                        // 속도 1만큼 증가
+                        // [스틱 횡방향 속도] 1만큼 증가
                         stickRoll += 1;
-                        // [-100 ~ 100] 순환
-                        if (stickRoll > 100) stickRoll = -100;
+                        // [0 ~ 200] 순환
+                        if (stickRoll > 200) stickRoll = 0;
 
-                        // 속도 1만큼 증가
+                        // [스틱 종방향 속도] 1만큼 증가
                         stickPitch += 1;
-                        // [-100 ~ 100] 순환
-                        if (stickPitch > 100) stickPitch = -100;
+                        // [0 ~ 200] 순환
+                        if (stickPitch > 200) stickPitch = 0;
 
-                        // 속도 1만큼 증가
+                        // [스틱 방위] 1만큼 증가
                         stickYaw += 1;
-                        // [-100 ~ 100] 순환
-                        if (stickYaw > 100) stickYaw = -100;
+                        // [0 ~ 200] 순환
+                        if (stickYaw > 200) stickYaw = 0;
+
+                        // 경도 1만큼 증가
+                        lonOfLP += 1;
+                        // [0 ~ 3600000000] 순환
+                        if (lonOfLP > 3600000000) lonOfLP = 0;
+                        //Console.WriteLine($"LonOfLP: {lonOfLP}, 고도(m): {(lonOfLP * 0.0000001) - 180.0}°(도)");
+
+                        // 위도 1만큼 증가
+                        latOfLP += 1;
+                        // [0 ~ 1800000000] 순환
+                        if (latOfLP > 1800000000) latOfLP = 0;
+                        //Console.WriteLine($"LatOfLP: {latOfLP}, 고도(m): {(latOfLP * 0.0000001) - 90.0}°(도)");
+
+                        // 고도 1만큼 증가
+                        altOfLP += 1;
+                        // [0 ~ 60000] 순환
+                        if (altOfLP > 60000) altOfLP = 0;
+                        //Console.WriteLine($"AltOfLP: {altOfLP}, 고도(m): {(altOfLP * 0.025) - 500.0} m");
                     }
 
                 }
